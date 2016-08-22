15445525a83d736e44347ad7cd4789a41c57789a
Agustín Carmona Díaz <agustin.carmona@gmail.com>
2016-08-22 16:06:43 +0200

    grunt clean added

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: {
+        src: ['dist/**/*'],
+        filter: 'isFile',
+      },
+    },
     concat: {
       options: {
         separator: ';'
@@ -42,14 +48,15 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
+  grunt.registerTask('test', ['clean', 'jshint', 'qunit']);
 
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['clean', 'jshint', 'qunit', 'concat', 'uglify']);
 
 };
